86878c43fdcf4550fd811deb9424e64cd4cb342a
Yorbit <marco.95112@hotmail.it>
2022-06-24 15:01:56 +0200

    base email add e test

--- Email/script.js
+++ Email/script.js
@@ -1,2 +1,16 @@
 const userMail = prompt('INSERISCI LA TUA EMAIL PER ACCEDERE');
 
+let found = false;
+let messageAccess = ('Spiacenti! Riprova')
+
+const emailAccess = ["simone@gmail.it", "rebecca@gmail.it", "andrea@gmail.it", "lorenzo@gmail.it"]
+
+for (let index = 0; index < emailAccess.length; index++) {
+
+    if (userMail == emailAccess[index]) {
+
+        found = true;
+
+    }
+}
+
