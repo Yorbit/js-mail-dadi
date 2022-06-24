e6b81b9e4476cb8fe80747ecf5eb731044762696
Yorbit <marco.95112@hotmail.it>
2022-06-24 15:53:23 +0200

    end

--- Dice/script.js
+++ Dice/script.js
@@ -4,3 +4,19 @@ const userNum = Math.floor(Math.random() * 6) + 1;
 
 const aiNum = Math.floor(Math.random() * 6) + 1;
 
+console.log(userNum);
+console.log(aiNum);
+
+if (userNum < aiNum) {
+
+    console.log('Hai perso..')
+
+} else if (userNum > aiNum) {
+
+    console.log('Hai vinto!!')
+
+} else {
+
+    console.log('Pari!');
+
+}
